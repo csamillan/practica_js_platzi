567d3b8666b80f04172ee327288aff6ac354959b
csamillan <csamillanm@gmail.com>
2022-02-15 18:31:58 -0500

    final calculo de figuras

--- funcGraficos.js
+++ funcGraficos.js
@@ -1,38 +1,97 @@
-// Codigo cuadrado
+function CalcularPerimetro(){
+    var lado1 = parseInt(document.getElementById("lado1").value);
+    var lado2 = parseInt(document.getElementById("lado2").value);
+    var lado3 = parseInt(document.getElementById("lado3").value);
+    var forma = document.getElementById("forma").value;
+    var vform = 0;
 
-const lcuadrado = 5;
-const pcuadrado = lcuadrado*4;
-const acuadrado = lcuadrado*lcuadrado;
+    if(forma == "c"){
+        vform = 1
+    }else if( forma == "t")
+    {
+        vform = 2;
+    }else{
+        vform =3;
+    };
 
-console.group("cuadrado");
-console.log(" El lado del cuadrado mide " + lcuadrado);
-console.log(" El perimetro del cuadrado mide " + pcuadrado);
-console.log(" El area del cuadrado mide " + acuadrado);
-console.groupEnd();
+    var result = perimetro(vform,lado1,lado2,lado3);
+    alert(result);
+}
 
-// Codigo Triangulo
+function CalcularArea(){
+    var lado1 = parseInt(document.getElementById("lado1").value);
+    var altura = parseInt(document.getElementById("altura").value);
+    var forma = document.getElementById("forma").value;
+    var vform = 0;
 
-const l1triangulo = 5;
-const l2triangulo = 5;
-const btriangulo = 5;
-const htriangulo = 5
-const ptriangulo = l1triangulo + l2triangulo + btriangulo;
-const atriangulo = (btriangulo*htriangulo)/2;
+    if(forma == "c"){
+        vform = 1
+    }else if( forma == "t")
+    {
+        vform = 2;
+    }else{
+        vform =3;
+    };
 
-console.group("Triangulo");
-console.log(" El lado del triangulo mide " + l1triangulo + "," + l2triangulo + "," + btriangulo);
-console.log(" El perimetro del triangulo mide " + ptriangulo);
-console.log(" El area del triangulo mide " + atriangulo);
-console.groupEnd();
+    var result = Area(vform,lado1,altura);
+    alert(result);
+}
 
-// Codigo Triangulo
+function perimetro(tipo,lado1,lado2,lado3){
 
-const radiocirculo = 5;
-const pcirculo = 2*radiocirculo*Math.PI;
-const acirculo = radiocirculo*radiocirculo*Math.PI;
+var perimetro = 0;
+var forma = "";
 
-console.group("circulo");
-console.log(" El radio del circulo mide " + radiocirculo);
-console.log(" El diametro del circulo mide " + pcirculo);
-console.log(" El area del circulo mide " + acirculo);
-console.groupEnd();
+    switch(tipo)
+    {
+        case 1: //triangulo
+            perimetro = lado1 * 4;
+            forma = "El perimetro del Cuadrado ";
+            break;
+
+        case 2://Triangulo
+            perimetro = lado1 + lado2 + lado3;
+            forma = "El perimetro del Triangulo ";
+            break;
+
+        case 3://Circulo
+            perimetro = 2*lado1*Math.PI;
+            forma = "El perimetro del Circulo ";
+            break;
+
+        default:
+            break;
+    }
+
+    return forma + perimetro + " cm"
+
+};
+
+function Area(tipo,lado1,altura)
+{
+    var perimetro = 0;
+    var forma = "";
+    
+        switch(tipo)
+        {
+            case 1: //triangulo
+                perimetro = lado1 * lado1;
+                forma = "El Area del Cuadrado";
+                break;
+    
+            case 2://Triangulo
+                perimetro = (lado1 *altura)/2;
+                forma = "El Area del Triangulo";
+                break;
+    
+            case 3://Circulo
+                perimetro = lado1*lado1*Math.PI;
+                forma = "El Area del Circulo";
+                break;
+    
+            default:
+                break;
+        }
+    
+        return forma + perimetro + " cm";
+};
